7503425ad9516b57098ade821951a16d2966b909
Danial Khoshkhou <danial.khoshkhou@voquis.com>
2022-04-17 11:01:32 +0100

    #4 Publish tagged builds to S3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if [[ $GITHUB_REF_TYPE = 'branch' ]]; then
+  # Always copy to
+  aws s3 cp lambda.zip "s3://voquis/aws/lambda_functions/contact-form-handler/${GITHUB_REF_NAME}.zip"
+fi
+
 if [[ $GITHUB_REF_TYPE = 'tag' ]]; then
   # Copy commit short SHA to tag
   aws s3 cp lambda.zip "s3://voquis/aws/lambda_functions/contact-form-handler/${GITHUB_REF_NAME}.zip"
